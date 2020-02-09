dd3b8ff66e61ec3b74d223909ede59f25e228228
linylmfos <iqianbaidu@163.com>
2020-02-09 12:43:18 +0800

    vue-nuxt-asyncDat3

--- pages/detail/_myid.vue
+++ pages/detail/_myid.vue
@@ -1,13 +1,37 @@
 <template>
-    <div>
-        detail--{{$route.params.myid}}
-    </div>
+  <div v-if="filminfo">
+    detail--{{$route.params.myid}}
+    <img :src="filminfo.poster"/>
+  </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
-    layout: "detailtemplate",
-    asyncData(data) {
-        console.log(data.params)
+  layout:"detailtemplate",
+
+  data(){
+    return {
+      filminfo:null
     }
+  },
+
+  asyncData(data) {
+    console.log(data.params)
+
+    return axios({
+      url:`https://m.maizuo.com/gateway?filmId=${data.params.myid}&k=7153258`,
+      headers:{
+        'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.0.4","e":"15610855429195524981146"}',
+        'X-Host': 'mall.film-ticket.film.info'
+      }
+    }).then(res=>{
+      return {
+        filminfo:res.data.data.film
+      }
+    })
+  },
 }
 </script>
+<style lang="scss" scoped>
+
+</style>
